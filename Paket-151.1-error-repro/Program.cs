fb9292e3b5ea0c08526135031141c88faa08c894
Marcin Deptuła <marcin.deptula@live.com>
2018-03-14 16:41:18 +0100

    Init commit

---
+++ Paket-151.1-error-repro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paket_151._1_error_repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var http = new HttpClient();
+            var resp = http.GetAsync("https://google.com").Result;
+        }
+    }
+}
